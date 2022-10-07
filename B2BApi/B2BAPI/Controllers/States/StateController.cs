61762c6a1eda3467941c5754891cf839ebd1a614
Mojtaba Khooryani <m.khooryani@outlook.com>
2022-10-07 18:16:26 +0200

    get-state-list API

---
+++ B2BApi/B2BAPI/Controllers/States/StateController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace B2BAPI.Controllers.States;
+
+[Route("")]
+[ApiController]
+public class StateController : ControllerBase
+{
+    [HttpPost("get-state-list")]
+    public IActionResult GetStateList()
+    {
+        return Ok(new StateDto[]
+        {
+            new StateDto("CA", "California"),
+            new StateDto("TX", "Texas"),
+        });
+    }
+}
---
+++ B2BApi/B2BAPI/Controllers/States/StateDto.cs
@@ -0,0 +1,3 @@
+﻿namespace B2BAPI.Controllers.States;
+
+public record StateDto(string Code, string Name);
