6680ad786d99b5c26c370b6fce140c8c34583fc8
Mojtaba Khooryani <m.khooryani@outlook.com>
2022-10-07 19:38:41 +0200

    get-bank-list API

---
+++ B2BApi/B2BAPI/Controllers/Banks/BankController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace B2BAPI.Controllers.Banks;
+
+[Route("")]
+[ApiController]
+public class BankController : ControllerBase
+{
+    [HttpPost("get-bank-list")]
+    public IActionResult GetBanksList([FromBody] BankListRequest _)
+    {
+        return Ok(new BankDto[]
+        {
+            new BankDto("JPMorgan Chase", "JPM"),
+            new BankDto("Citigroup", "CITI"),
+        });
+    }
+}
---
+++ B2BApi/B2BAPI/Controllers/Banks/BankDto.cs
@@ -0,0 +1,3 @@
+﻿namespace B2BAPI.Controllers.Banks;
+
+public record BankDto(string Name, string Code);
