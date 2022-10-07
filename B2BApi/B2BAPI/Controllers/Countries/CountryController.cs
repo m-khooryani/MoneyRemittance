45c65a5b9613a5c741fb3333c39e2b22bba03f0d
Mojtaba Khooryani <m.khooryani@outlook.com>
2022-10-07 16:39:46 +0200

    Add project files.

---
+++ B2BApi/B2BAPI/Controllers/Countries/CountryController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace B2BAPI.Controllers.Countries;
+
+//[Route("api/countries")]
+[ApiController]
+public class CountryController : ControllerBase
+{
+    [HttpPost("get-country-list")]
+    [ProducesResponseType(typeof(void), statusCode: 200)]
+    public IActionResult GetCountries()
+    {
+        return Ok("success");
+    }
+}
